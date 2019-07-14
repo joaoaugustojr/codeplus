b550f68bc4a55548b21bd3923a1aa093a6fa36bf
joaoaugustjr <jhonaugustjunior@gmail.com>
2019-07-14 00:54:38 -0300

    tela login css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,41 @@
     {{'codeplus - Login'}}
 @endsection
 
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/stylesLogin.css') }}">
+@endsection
+
 @section('content')
 
 <section class="container-fluid" >
 
-    <section class="dados d-flex justify-content-center align-items-center">
+    <section class="loginBox bg-white text-center">
 
-        <div class="logo">
-            <h1>teste 1</h1>
+        <div class="codePlusLogo text-center d-inline-block justify-content-center">
+            <img class="text-center" src="{{ asset('src/images/logocodeplus.png') }}">
+            <ion-icon class="dropIcon" name="arrow-dropdown"></ion-icon>
         </div>
 
-        {!! Form::open(['route' => 'rt.telaLogin', 'method' => 'post']) !!}
-        {!! Form::close() !!}
+        <form class="d-inline-block formLogin" action="" method="post">
+
+            <div class="groupInput">
+                <div class="iconLogin"><ion-icon name="apps"></ion-icon></div>
+                <input type="text" name="estabelecimento" id="" placeholder="Código do Estabelecimento">
+            </div>
+            <div class="groupInput">
+                <div class="iconLogin"><ion-icon name="person"></ion-icon></div>
+                <input type="text" name="login" id="" placeholder="Usuário">
+            </div>
+            <div class="groupInput">
+                <div class="iconLogin"><ion-icon name="key"></ion-icon></div>
+                <input type="password" name="password" id="" placeholder="Senha">
+            </div>
+
+            <button class="btnLogin" type="submit">Login</button>
+        </form>
+
+        <p class="text-capitalize textSuport"><small class="font-weight-light">não consegue entrar? contate suporte</small></p>
+
 
     </section>
 
